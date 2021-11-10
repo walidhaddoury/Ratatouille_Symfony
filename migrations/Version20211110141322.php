f53094bcc973f7b9d44914f47132cb4bf6d5fe86
Océane Le Goff <oceanelegoff@MacBook-Pro-de-Oceane.local>
2021-11-10 15:17:45 +0100

    modif spent + activity + edit/create compte

---
+++ migrations/Version20211110141322.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211110141322 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE activity_user (activity_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_8E570DDB81C06096 (activity_id), INDEX IDX_8E570DDBA76ED395 (user_id), PRIMARY KEY(activity_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE activity_user ADD CONSTRAINT FK_8E570DDB81C06096 FOREIGN KEY (activity_id) REFERENCES activity (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE activity_user ADD CONSTRAINT FK_8E570DDBA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE spent ADD user_id INT NOT NULL');
+        $this->addSql('ALTER TABLE spent ADD CONSTRAINT FK_F1653759A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_F1653759A76ED395 ON spent (user_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE activity_user');
+        $this->addSql('ALTER TABLE spent DROP FOREIGN KEY FK_F1653759A76ED395');
+        $this->addSql('DROP INDEX IDX_F1653759A76ED395 ON spent');
+        $this->addSql('ALTER TABLE spent DROP user_id');
+    }
+}
