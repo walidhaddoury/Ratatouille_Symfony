15832211c0316bb01c589bfb50926cabbc6797fc
Walid <walidhaddoury@outlook.fr>
2021-11-10 16:50:18 +0100

    formulaire d'inscription fini

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +16,49 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('first_name')
-            ->add('last_name')
-            ->add('mail')
-            ->add('picture')
-            ->add('password')
+            ->add('first_name',
+                TextType::class,
+                [
+                    'required' => true,
+                    'attr' => [
+                        'class' => "p-3 mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0",
+                        'maxlength' => 32
+                    ]
+                ]
+            )
+            ->add('last_name',
+                TextType::class,
+                [
+                    'required' => true,
+                    'attr' => [
+                        'class' => "p-3 mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0",
+                        'maxlength' => 32
+                    ]
+                ])
+            ->add('mail',
+                EmailType::class,
+                [
+                    'required' => true,
+                    'attr' => [
+                        'class' => "p-3 mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0",
+                        'maxlength' => 100
+                    ]
+                ])
+            ->add('picture',
+                FileType::class,
+                [
+                    'required' => false,
+                ])
+            ->add('password',
+                PasswordType::class,
+                [
+                    'required' => true,
+                    'attr' => [
+                        'class' => "p-3 mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0",
+                        'maxlength' => 100,
+                        'type' => "password"
+                    ]
+                ])
         ;
     }
 
