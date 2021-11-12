50797fed008453a78316a9106733af7f3f53de9f
Océane Le Goff <oceanelegoff@MacBook-Pro-de-Oceane.local>
2021-11-12 10:13:07 +0100

    modif user, spent, activity

--- src/Form/SpentType.php
+++ src/Form/SpentType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Spent;
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +15,26 @@ class SpentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('expense_name')
-            ->add('cost')
+            ->add('expense_name',
+                TextType::class,
+                [
+                    'required' => true,
+                    'attr' => [
+                        'placeholder' => "Name",
+                        'class' => "p-3 mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0",
+                        'maxlength' => 100
+                    ]
+                ])
+            ->add('cost',
+                IntegerType::class,
+                [
+                    'required' => true,
+                    'attr' => [
+                        'placeholder' => "Cost",
+                        'class' => "p-3 mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0",
+                        'maxlength' => 100
+                    ]
+                ])
         ;
     }
 
